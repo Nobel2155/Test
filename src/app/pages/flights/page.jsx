fe8d8c41c291d97e6dbcf938fcb091bc56992478
azmain-siam <siamhossain2700@gmail.com>
2024-08-02 10:14:16 +0600

    Add: Flights Feature section

--- src/app/pages/flights/page.jsx
+++ src/app/pages/flights/page.jsx
@@ -1,9 +1,15 @@
 import FlightBanner from "@/components/flightsHome/banner/FlightBanner";
+import FlightFeatures from "@/components/flightsHome/featuresSection/FlightFeatures";
+import FlightRating from "@/components/flightsHome/FlightRating";
 
 const FlightsHome = () => {
   return (
     <div>
       <FlightBanner />
+      <div className="container max-w-[1200px] mx-auto">
+        <FlightFeatures />
+        <FlightRating />
+      </div>
     </div>
   );
 };
